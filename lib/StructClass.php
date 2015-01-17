94e381ef05051cb4cb4b12cd64940f20ee5c32fc
Tolan Blundell <tolan@patternseek.net>
2015-01-17 14:50:02 +0100

    Import

---
+++ lib/StructClass.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * This file is part of the Patternseek StructClass library.
+ *
+ * (c)2015 Tolan Blundell <tolan@patternseek.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PatternSeek\StructClass;
+
+class StructClass
+{
+
+    /**
+     * Disable get of non-existent properties.
+     * @param $property
+     * @throws \Exception
+     */
+    public function __get( $property )
+    {
+        throw new \Exception( "Non-existent property {$property} get in " . get_class( $this ) );
+    }
+
+    /**
+     * Disable set of non-existent properties.
+     * @param $property
+     * @param $value
+     * @throws \Exception
+     */
+    public function __set( $property, $value )
+    {
+        throw new \Exception( "Non-existent property {$property} set in " . get_class( $this ) );
+    }
+
+    /**
+     * Populate the StructClass's properties from an array
+     * @param array $properties
+     */
+    static function fromArray( array $properties ){
+        $selfClass = get_called_class();
+        $obj = new $selfClass();
+        foreach( $properties as $property=>$value ){
+            $obj->$$property = $value;
+        }
+        return $obj;
+    }
+
+}
